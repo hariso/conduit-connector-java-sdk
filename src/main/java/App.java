124d5c228641a3ef98c3e615eef209d6dedfda86
Haris Osmanagic <haris@meroxa.io>
2023-05-09 17:51:48 +0200

    add proto, bootstrap

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class App {
+    public static void main(String... args) {
+        int port = 9000; // default
+        System.out.printf("1|1|tcp|localhost:%d|grpc%n", port);
+        Quarkus.run(args);
+    }
+}
